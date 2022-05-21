4b819eca206b607136a9a3cc969eade5c9ac4a45
HeebongYu <gmlqhd94@gmail.com>
2022-05-21 16:34:01 +0900

    2022.05.21

--- ver2/assets/js/open.js
+++ ver2/assets/js/open.js
@@ -3,8 +3,7 @@ const openPage = openWrap.querySelector(".open__page");
 const openCanvas = openPage.querySelector(".open__canvas");
 const openTitle = openPage.querySelector(".open__title");
 const openButton = openPage.querySelector(".open__button");
-const openLoading = openWrap.querySelector(".open__loading");
-const loadingTxt = openLoading.querySelector(".txt");
+const openButtonTxt = openPage.querySelector(".open__button span");
 
 openButton.addEventListener("click", () => {
   loading();
@@ -24,6 +23,13 @@ function loading() {
     ease: "expo.inOut",
     delay: 0.5,
   });
+  // openButtonTxt 제거
+  gsap.to(openButtonTxt, {
+    duration: 1,
+    opacity: 0,
+    ease: "expo.inOut",
+    delay: 0.5,
+  });
   // openButton 제거
   gsap.to(openButton, {
     duration: 1.5,
@@ -39,33 +45,8 @@ function loading() {
     ease: "expo.inOut",
     delay: 1.5,
   });
-  // openPage 제거
-  gsap.to(openPage, {
-    display: "none",
-    delay: 3,
-  });
-  // openLoading 생성
-  gsap.to(openLoading, {
-    display: "flex",
-    delay: 3,
-  });
-  // 로딩 타이핑
-  gsap.to(loadingTxt, {
-    duration: 1,
-    opacity: 1,
-    ease: "expo.inOut",
-    delay: 3,
-  });
-  // loadingTxt 제거
-  gsap.to(loadingTxt, {
-    duration: 1,
-    opacity: 0,
-    ease: "expo.inOut",
-    delay: 5.5,
-    css: { className: "animate__animated animate__zoomOutUp txt" },
-  });
   // intro 페이지로 이동
   setTimeout(() => {
     location.href = "./pages/intro.html";
-  }, 6500);
+  }, 3000);
 }
--- ver2/assets/js/version.js
+++ ver2/assets/js/version.js
@@ -2,7 +2,7 @@ document.body.onload = addElement;
 
 function addElement() {
   let newDiv = document.createElement("div");
-  let newContent = document.createTextNode("ver.2 2022.05.20.2'");
+  let newContent = document.createTextNode("ver.2.0521.00");
   newDiv.appendChild(newContent);
   newDiv.classList.add("version");
 
