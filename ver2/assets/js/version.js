8ea8190aad6b047abce8367d106f4a8468c422fc
HeebongYu <gmlqhd94@gmail.com>
2022-05-22 17:03:57 +0900

    0521.99

--- ver2/assets/js/version.js
+++ ver2/assets/js/version.js
@@ -2,7 +2,7 @@ document.body.onload = addElement;
 
 function addElement() {
   let newDiv = document.createElement("div");
-  let newContent = document.createTextNode("ver.2.0521.02");
+  let newContent = document.createTextNode("ver.2.0521.99");
   newDiv.appendChild(newContent);
   newDiv.classList.add("version");
 
