457231f76a2e214ebae66eb2ca0d58fca034b47b
HeebongYu <gmlqhd94@gmail.com>
2022-05-23 10:52:43 +0900

    22.05.23.4

--- ver2/assets/js/open.js
+++ ver2/assets/js/open.js
@@ -5,50 +5,55 @@ const openTitle = openPage.querySelector(".open__title");
 const openButton = openPage.querySelector(".open__button");
 const openButtonTxt = openPage.querySelector(".open__button span");
 
+(function () {
+  // location.href = "./index.html";
+  return;
+})();
+
 openButton.addEventListener("click", () => {
   loading();
 });
 
 function loading() {
   // openCanvas 제거
-  gsap.to(openCanvas, {
-    duration: 1.5,
-    opacity: 0,
-    ease: "expo.inOut",
-  });
-  // openTitle 제거
-  gsap.to(openTitle, {
-    duration: 1.5,
-    opacity: 0,
-    ease: "expo.inOut",
-    delay: 0.5,
-  });
-  // openButtonTxt 제거
-  gsap.to(openButtonTxt, {
-    duration: 1,
-    opacity: 0,
-    ease: "expo.inOut",
-    delay: 0.5,
-  });
-  // openButton 제거
-  gsap.to(openButton, {
-    duration: 1.5,
-    width: 0,
-    opacity: 0,
-    ease: "expo.inOut",
-    delay: 1,
-  });
-  // 배경 변경
-  gsap.to(openWrap, {
-    duration: 1.5,
-    backgroundColor: "#c1d72e",
-    ease: "expo.inOut",
-    delay: 1.5,
-  });
-  // intro 페이지로 이동
   setTimeout(() => {
-    location.href = "./pages/intro.html";
-  }, 3000);
+    gsap.to(openCanvas, {
+      duration: 1.5,
+      opacity: 0,
+      ease: "expo.inOut",
+    });
+    // openTitle 제거
+    gsap.to(openTitle, {
+      duration: 1.5,
+      opacity: 0,
+      ease: "expo.inOut",
+      delay: 0.5,
+    });
+    // openButtonTxt 제거
+    gsap.to(openButtonTxt, {
+      duration: 1,
+      opacity: 0,
+      ease: "expo.inOut",
+      delay: 0.5,
+    });
+    // openButton 제거
+    gsap.to(openButton, {
+      duration: 1.5,
+      width: 0,
+      opacity: 0,
+      ease: "expo.inOut",
+      delay: 1,
+    });
+    // 배경 변경
+    gsap.to(openWrap, {
+      duration: 1.5,
+      backgroundColor: "#c1d72e",
+      ease: "expo.inOut",
+      delay: 1.5,
+    });
+    // intro 페이지로 이동
+    setTimeout(() => {
+      location.href = "./pages/intro.html";
+    }, 3000);
+  });
 }
-
-gsap.restart();
